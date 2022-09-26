46ddd98ea570c429b42837def98f62592541541b
Sherzod <sherzodd7707@gmail.com>
2022-09-26 23:13:15 +0500

    Third Attempt

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -12,7 +12,8 @@ public class PrePostIncrement {
         // use post-increment operator in the mathematical expression  
         int x =   ++a   + b +   c   + d;
         System.out.println("Expected x is 40, x = " + x);
-        // print the updated value of a, b, c, and d  
+        // print the updated value of a, b, c, and d
+        int  s=++c;
         System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
 
     }
