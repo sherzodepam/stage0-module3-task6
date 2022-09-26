3b05e4a3dd524e36cdc44244a2c1b4e4d9c35902
Sherzod <sherzodd7707@gmail.com>
2022-09-26 13:21:36 +0500

    First Attempt

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -3,9 +3,11 @@ package lang.print.gaps.task6;
 public class PrePostIncrement {
     public static void main(String[] args) {
         int a = 5;
-        int b = 7;
+        int b= 7;
         int c = 12;
         int d = 15;
+        int f=b++;
+
 
         // use post-increment operator in the mathematical expression  
         int x =   a   + b +   c   + d;
