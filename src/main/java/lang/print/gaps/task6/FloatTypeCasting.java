9bf2c2fb98bdbc6ebe7a307c4a0f647afbde2fcb
Name Surname <StudentID@newuzbekistanuniversity.uz>
2022-12-15 12:16:08 +0500

    again

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int number= (int) 19.4F;
-        System.out.println(number);
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
